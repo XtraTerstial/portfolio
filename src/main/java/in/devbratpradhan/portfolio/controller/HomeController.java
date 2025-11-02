1850c2971f75e5fdffa45aee941d4f258874f11d
Devbrat Pradhan <devbratpradhan40@gmail.com>
2025-11-03 00:32:07 +0530

    v1.4

--- src/main/java/in/devbratpradhan/portfolio/controller/HomeController.java
+++ src/main/java/in/devbratpradhan/portfolio/controller/HomeController.java
@@ -1,17 +1,21 @@
 package in.devbratpradhan.portfolio.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
+    @Value("${web3forms.access_key}")
+    private String web3formsKey;
+
     @GetMapping({"","/","/home"})
-    public String showHomePage(){
+    public String showHomePage(Model model){
+        model.addAttribute("web3formsKey", web3formsKey);
         return "home";
     }
-
-
 }
